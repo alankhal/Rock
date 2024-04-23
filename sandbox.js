f3fc763cc5b21060160940ba7159b9afcfc40cb2
Aloon <149541930+alankhal@users.noreply.github.com>
2024-04-22 23:02:14 -0500

    First Commit
    Initial template at setting the rock paper scissor game without images or having the bot set at winning.

---
+++ sandbox.js
@@ -0,0 +1,15 @@
+// First set up the logic of the game 
+const game = () => {
+    let playerScore = 0;
+    let computerScore 0;
+    let moves = 0;
+
+    //Function to 
+    const playGame = () => {
+        
+    } 
+
+
+}
+
+    //Function to 
